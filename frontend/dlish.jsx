f7bbb3075544d90c177cbd9c11dbdcc3ee746419
Alex Park <apark0720@gmail.com>
2017-07-18 11:14:19 -0700

    finish auth backend

--- frontend/dlish.jsx
+++ frontend/dlish.jsx
@@ -14,5 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //   store = configureStore();
   // }
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<h1> Welcome </h1>, root);
 });
+
+// <Root store={store} />
