836ececb9262d0f5d626a646cc57b2fdfb399a1c
Alex Park <apark0720@gmail.com>
2017-07-23 13:56:25 -0700

    fix splash page images and banner alignment

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -7,15 +7,17 @@ import Footer from '../home/footer';
 
 const Home = () => (
   <section>
-
+    <NavbarContainer />
     <main>
-      <NavbarContainer />
-      <div className="homepage-video">
-        <video loop autoPlay>
-          <source src="http://res.cloudinary.com/apark0720/video/upload/v1500767920/dlishbanner_ylseil.mp4" type="video/mp4" />
-        </video>
-        <h1>Any recipe in the world, <br/> delivered to your office.</h1>
+      <div>
+        <div className="homepage-video">
+          <video loop autoPlay>
+            <source src="http://res.cloudinary.com/apark0720/video/upload/v1500767920/dlishbanner_ylseil.mp4" type="video/mp4" />
+          </video>
+          <h1>Any recipe in the world, <br/> delivered to your office.</h1>
+        </div>
       </div>
+
 
       <div className="homepage-container">
         <div className="homepage-content">
@@ -30,12 +32,12 @@ const Home = () => (
 
           <div className="homepage-body">
             <div className="homepage-entry">
-              <img className="homepage-image-1" src={"http://res.cloudinary.com/apark0720/image/upload/v1500735174/1stpic_tg7wjj.png"} alt="cartoon-figures" />
+              <img className="homepage-image-1" src={"http://res.cloudinary.com/apark0720/image/upload/v1500784758/pic1_fjd1mz.png"} alt="cartoon-figures" />
 
               <div className="homepage-blurb">
                 <h2>Submit a Recipe</h2>
                 <p>
-                  Found a mouthwatering recipe but don't want to make it yourself? Submit a link of the recipe and designate a time and place for delivery.
+                  Found a mouthwatering recipe but don't want to make it yourself? We got you covered! Simply submit a link of the recipe and designate a time and place for delivery.
                 </p>
               </div>
 
@@ -46,19 +48,19 @@ const Home = () => (
               <div className="homepage-blurb">
                 <h2>Get notified within 24 hours</h2>
                 <p>
-                  If our chefs can make the dish, you will receive an email and your recipe will appear on our menu. We partner with dozens of top chefs that specialize in everything from Thai to BBQ. The best chef for the job will add a personal touch and handcraft each meal.
+                  If our chefs can make the dish, you will receive an email and your recipe will appear on our menu. We partner with dozens of top chefs that specialize in everything from authentic Thai cuisine to Texas BBQ. The best chef for the job will add a personal touch and handcraft each meal.
                 </p>
               </div>
-              <img className="homepage-image-2" src={"http://res.cloudinary.com/apark0720/image/upload/v1500735176/2ndpic_wfnaoo.png"} alt="cartoon-figures" />
+              <img className="homepage-image-2" src={"http://res.cloudinary.com/apark0720/image/upload/v1500784759/pic2_j3npyc.jpg"} alt="cartoon-figures" />
             </div>
 
 
             <div className="homepage-entry">
-              <img className="homepage-image-3" src={"http://res.cloudinary.com/apark0720/image/upload/v1500735178/3rd_pic_sqh8gy.png"} alt="cartoon-figures" />
+              <img className="homepage-image-3" src={"http://res.cloudinary.com/apark0720/image/upload/v1500785608/pic3_tm8jas.jpg"} alt="cartoon-figures" />
               <div className="homepage-blurb">
                 <h2>The more the merrier!</h2>
                 <p>
-                  The more people who join your order, the cheaper your meal will be. Invite your coworkers and friends to join you!
+                  The more people who join your order, the cheaper your meal will be. Invite your coworkers and friends to join you for lunch!
                 </p>
               </div>
             </div>
